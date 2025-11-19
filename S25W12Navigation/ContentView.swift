127c865110539288094cc39e239054a541124b3d
20210181 <20210181@kumoh.ac.kr>
2025-11-19 15:01:03 +0900

    ✨Detail View

--- S25W12Navigation/ContentView.swift
+++ S25W12Navigation/ContentView.swift
@@ -1,20 +1,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = SongViewModel()
+    
     var body: some View {
         NavigationStack {
-            Text("노래 화면")
-
-            NavigationLink(destination: SingerView()) {
-                Text("가수 화면으로 이동")
+            List(viewModel.songs) { song in
+                NavigationLink(destination: SongDetailView(song: song)) {
+                    VStack(alignment: .leading) {
+                        Text(song.title)
+                            .font(.headline)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(song.singer)
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
             }
         }
     }
 }
 
-struct SingerView: View {
+//struct SingerView: View {
+//    var body: some View {
+//        Text("가수 화면")
+//    }
+//}
+
+struct SongDetailView: View {
+    let song: Song
+
     var body: some View {
-        Text("가수 화면")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 15) {
+                HStack {
+                    Text(song.singer)
+                        .font(.title2)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                    Text(String(song.rating))
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                }
+                .padding(.bottom, 10)
+
+                Text(song.lyrics)
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+            }
+            .padding()
+        }
+        .navigationTitle(song.title)
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
